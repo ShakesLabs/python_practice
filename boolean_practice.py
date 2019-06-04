86e06dbdff0522ed8a18e4eb64c2c9aeac7793ac
Lil Debbie <sarahdeborahallen@gmail.com>
2019-06-03 21:21:12 -0500

    hi

---
+++ boolean_practice.py
@@ -0,0 +1,10 @@
+print("Type a true or false value")
+value = input()
+
+if value == "False":
+    print ("This is not true")
+elif value == "True":
+    print ("This is true")
+else:
+    print ("Invalid input")
+
