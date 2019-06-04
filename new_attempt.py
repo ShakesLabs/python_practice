745cfd7254db02d564bd4b9c8f81e3440a145c74
Lil Debbie <sarahdeborahallen@gmail.com>
2019-06-03 21:36:07 -0500

    we did it fam!

---
+++ new_attempt.py
@@ -0,0 +1,11 @@
+print("Which twin is better, Sarah or Rebekah?") 
+value = input()
+
+if value == "Sarah":
+    print ("You are correct")
+elif value == "Rebekah":
+    print ("you are worthless")
+else: 
+    print ("try again")
+
+
